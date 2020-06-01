283e3b9fa8e80be8add42a9f84af76154194ba02
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2020-06-01 14:02:28 +0900

    set-before-action-login?

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,16 @@
 class TweetsController < ApplicationController
 
+  before_action :login?
+
   def index
+  end
+
+  private
+
+  def login?
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 
 end
