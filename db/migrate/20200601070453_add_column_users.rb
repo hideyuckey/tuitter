28f442593b0e39f405d392bf767ff70a450db010
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2020-06-01 16:59:48 +0900

    add-view-of-signup

--- db/migrate/20200601070453_add_column_users.rb
+++ db/migrate/20200601070453_add_column_users.rb
@@ -1,8 +1,7 @@
 class AddColumnUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :nickname, :string, null: false, after: :id
-    add_column :users, :account, :string, null: false, unique: true, after: :name
-    add_column :users, :icon, :string, after: :account
+    add_column :users, :username, :string, null: false, after: :id
+    add_column :users, :icon, :string, after: :username
     add_column :users, :image, :string, after: :icon
     add_column :users, :text, :text, after: :image
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_06_01_070453) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "name", null: false
-    t.string "account", null: false
+    t.string "username", null: false
     t.string "icon"
     t.string "image"
     t.text "text"
