52523b2e20ab3df9e40a1c898ff636bbece0b7c3
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2020-06-03 13:30:28 +0900

    can-send-messages

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,9 +3,18 @@ class TweetsController < ApplicationController
   before_action :login?
 
   def index
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to action: :index
   end
 
   private
+  def tweet_params
+    params.require(:tweet).permit(:message, :image).merge(user_id: current_user.id)
+  end
 
   def login?
     unless user_signed_in?
