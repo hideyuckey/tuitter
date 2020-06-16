8f4003c40351f562b17039de74563f1d8bbb6b4e
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2020-06-16 09:47:31 +0900

    can-show-preview

--- app/assets/javascripts/image-preview.js
+++ app/assets/javascripts/image-preview.js
@@ -25,4 +25,31 @@ $(document).on('turbolinks:load', function(){
       }
     })
   })
+
+  $(function(){
+    function buildHTML(icon){
+      var html =
+        `
+        <div class = 'prev-icon-content'>
+          <img src = "${icon}", class = 'prev-icon'>
+        <div>
+        `
+      return html;
+    }
+
+    $(document).on("change", '.form-icon__hiden', function(){
+      var file = this.files[0];
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function(){
+        var icon = this.result;
+        if ($(".prev-icon-content").length == 0){
+          var html = buildHTML(icon);
+          $(".prev-icon-contents").prepend(html);
+        } else {
+          $(".prev-icon-content .prev-icon").attr({src: icon});
+        }
+      }
+    })
+  })
 })
