25d8ce547517881e7a5beaa932aecb582b896dc4
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2020-06-19 09:39:34 +0900

    change-tweet/good-column-edfault0

--- db/migrate/20200618010459_add_column_to_tweets.rb
+++ db/migrate/20200618010459_add_column_to_tweets.rb
@@ -1,5 +1,5 @@
 class AddColumnToTweets < ActiveRecord::Migration[5.2]
   def change
-    add_column :tweets, :good, :integer, after: :image
+    add_column :tweets, :good, :integer, after: :image, default: 0
   end
 end
