1abee069e2bfe73c9c0d2b4fffa6eaa28a47dda0
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2020-08-12 16:33:18 +0900

    edit-deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,14 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: Rails.application.credentials.aws[:access_key_id],
+  AWS_SECRET_ACCESS_KEY: Rails.application.credentials.aws[:secret_access_key]
+}
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 after 'deploy:publishing', 'deploy:restart'
   namespace :deploy do
   task :restart do
