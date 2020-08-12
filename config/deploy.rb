ac9b253629a80a3ab5e0ea36e014ffa4889c70cc
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2020-08-12 16:55:21 +0900

    modi-deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,8 @@ set :keep_releases, 5
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: Rails.application.credentials.aws[:access_key_id],
-  AWS_SECRET_ACCESS_KEY: Rails.application.credentials.aws[:secret_access_key]
+  aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+  aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
 }
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
