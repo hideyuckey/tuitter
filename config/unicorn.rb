beacf89437ed5bf669e59df4276d2fbb4686a799
hideyuckey <54564032+hideyuckey@users.noreply.github.com>
2020-08-17 11:02:49 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 check_client_connection false
 run_once = true
 before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = @app_path + "/current/Gemfile"
+  ENV["BUNDLE_GEMFILE"] = "/path/to/app/current/Gemfile"
 end
 before_fork do |server, worker|
  defined?(ActiveRecord::Base) &&
